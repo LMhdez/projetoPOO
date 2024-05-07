d9eac7c713ea9573cc414fb1e8e73e7e454ad52b
luish <luismariohernandezsantos@gmail.com>
2024-05-07 10:11:11 +0100

    farmaceutico

---
+++ src/Farmaceutico.java
@@ -0,0 +1,14 @@
+public class Farmaceutico extends User{
+    private String NIF;
+    private String morada;
+    private String contato;
+
+    Farmaceutico(String aLogin, String aPassword, String aNome, String aEmail, boolean aAtivo, String aNIF, String aMorada,
+            String aContato) {
+        super(aLogin, aPassword, aNome, aEmail, aAtivo);
+        this.NIF = aNIF;
+        this.morada = aMorada;
+        this.contato = aContato;
+
+    }
+}
--- src/GerirUser.java
+++ src/GerirUser.java
@@ -2,17 +2,29 @@ import java.util.ArrayList;
 
 public class GerirUser {
 
-    private ArrayList<User> lista;
+  private ArrayList<User> lista;
 
-    public GerirUser() {
-        lista = new ArrayList<User>();
-    }
+  public GerirUser() {
+    lista = new ArrayList<User>();
+  }
 
-    public boolean adicionaUser(User aUser) {
-        if (lista != null)
-            return lista.add(aUser);
-        return false;
-    }
+  public boolean adicionaUser(User aUser) {
+    if (lista != null)
+      return lista.add(aUser);
+    return false;
+  }
 
-    
+  public boolean criaGestor(String login, String password, String nome, String email, boolean aAtivo) {
+    Gestor gestor = new Gestor(login, password, nome, email, aAtivo);
+    return adicionaUser(gestor);
+  }
+
+  public boolean criaFarmaceutico(String aLogin, String aPassword, String aNome, String aEmail, boolean aAtivo,
+      String aNIF, String aMorada,
+      String aContato) {
+
+    Farmaceutico farmaceutico = new Farmaceutico(aLogin, aPassword, aNome, aEmail, aAtivo, aNIF, aMorada, aContato);
+    return adicionaUser(farmaceutico);
+  }
+
 }
