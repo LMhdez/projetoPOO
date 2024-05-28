cbf4560db61cd06dd9cea37a0aa9018163b2639f
luish <luismariohernandezsantos@gmail.com>
2024-05-28 11:39:01 +0100

    funcoes desnecessarias

--- src/Gestor.java
+++ src/Gestor.java
@@ -4,12 +4,7 @@ public class Gestor extends User {
         super(login, password, nome, email, ativo);
     }
 
-    public boolean aprovarEncomenda(Encomendas aEncomenda){
-        return aEncomenda.setAprovado();
-    }
-    public boolean encerrarEncomenda(Encomendas aEncomenda){ 
-        return aEncomenda.setEncerrado();
-    }
+ 
     public  boolean AssociarFarmaceutico(Encomendas aEncomenda, Farmaceutico aFarmaceutico){ 
         return aEncomenda.setFarmaceutico(aFarmaceutico);
     }
